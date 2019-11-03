832ceef81a4ce334d15313565fb2a328b78894e7
sbx_user1051 <sbx_user1051@169.254.104.13>
2019-11-03 17:34:04 +0000

    Sun Nov 03 17:34:04 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,13 +6,13 @@ def checkout(skus):
     # initializing price variable
     price = 0
     #defining items dictionary
-    dict_products =
-    {
+    dict_products = {
         'A': 50,
         'B': 30,
         'C': 20,
         'D': 15
     }
-
+    dict_discount_quantity = { 'A': 3, 'B': 2 }
+    dict_discount_price = {'A': 130, 'B': 45 }
 
 
