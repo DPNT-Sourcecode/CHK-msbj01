a91167283e8004a9e970318edede4d3b69d40bb1
sbx_user1051 <sbx_user1051@169.254.96.165>
2019-11-04 18:07:18 +0000

    Mon Nov 04 18:07:18 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,38 +1,45 @@
-import re
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     try:
-        # initializing price variable
-        price = 0
-        #defining items dictionary
-        dict_products = {
-            'A': 50,
-            'B': 30,
-            'C': 20,
-            'D': 15
-        }
-        dict_discount_quantity = {'A': 3, 'B': 2}
-        dict_discount_price = {'A': 130, 'B': 45}
+        if skus.isalpha():
+            # initializing price variable
+            price = 0
+            #defining items dictionary
+            dict_products = {
+                'A': 50,
+                'B': 30,
+                'C': 20,
+                'D': 15
+            }
+            dict_discount_quantity = {'A': 3, 'B': 2}
+            dict_discount_price = {'A': 130, 'B': 45}
 
-        lst_cart = skus.split(',')
-        for elem in lst_cart:
-            final_price = 0
-            extract_num = re.findall('\d+', elem)
-            cnt_product = int(extract_num[0])
-            extract_str = elem.replace(extract_num[0],'')
+            unique_items_cart = ''.join(set(skus))
+            for elem in unique_items_cart:
+                if elem in dict_products:
+                    final_price = 0
+                    cnt_product = skus.count(elem)
+                    if elem in dict_discount_quantity:
+                        discount_price = int()
+                        discount_price =  int(cnt_product / dict_discount_quantity[elem] * dict_discount_price[elem])
+                        no_discount_price = int((cnt_product % dict_discount_quantity[elem]) * dict_products[elem])
+                        final_price = discount_price + no_discount_price
+                    else:
+                        final_price = cnt_product * dict_products[elem]
+                    price += int(final_price)
+                else:
+                    price = -1
+                    break
+        else:
+            price = -1
 
-            if extract_str in dict_discount_quantity:
-                x = int(cnt_product / dict_discount_quantity[extract_str])
-                discount_price =  x * dict_discount_price[extract_str]
-                no_discount_price = (cnt_product % dict_discount_quantity[extract_str]) * dict_products[extract_str]
-                final_price = discount_price + no_discount_price
-            else:
-                final_price = cnt_product * dict_products[extract_str]
+        return price
 
-            price += int(final_price)
     except Exception as e:
         price = str(e)
 
     return price
+
+total_price = checkout('AAAAAAAAAABBBBBCCCDDDD')
+print (total_price)
