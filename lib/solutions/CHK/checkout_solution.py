54a5341e0deba6e536f3bbca7b9d1e71fead62ab
sbx_user1051 <sbx_user1051@169.254.4.69>
2019-11-09 14:07:02 +0000

    Sat Nov 09 14:07:02 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,7 +17,7 @@ def checkout(skus):
                 'E': 40,
                 'F': 10
             }
-            dict_discount_quantity = {'A': [(5,200), (3,130)], 'B': [(2, 45)]}
+            dict_discount_quantity = {'A': [(5,200), (3,130)], 'B': [(2, 45)], 'F': [(1,10)]}
             special_discount = {'B': [(2,'E')], 'F': [(2, 'F')]}
 
             unique_items_cart = ''.join(set(skus))
@@ -63,4 +63,5 @@ def checkout(skus):
 mycart = 'AAAAAAAABBBCCCDDDDEEEEEFFFFF'
 totalprice = checkout(mycart)
 print (totalprice)
+
 
