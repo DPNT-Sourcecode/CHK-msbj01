268104c64093721c32c6d4c0370237d7ddee3f91
sbx_user1051 <sbx_user1051@169.254.4.69>
2019-11-09 13:35:08 +0000

    Sat Nov 09 13:35:08 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,8 +25,22 @@ def checkout(skus):
                     final_price = tmp_price = discount_price = 0
                     cnt_product = get_count(elem)
                     if elem in dict_discount_quantity:
-                        discount_price =  int(int(cnt_product / dict_discount_quantity[elem]) * dict_discount_price[elem])
-                        no_discount_price = int((cnt_product % dict_discount_quantity[elem]) * dict_products[elem])
+                        for val in dict_discount_quantity[elem]:
+                            if elem in special_discount:
+                                for sdval in special_discount[elem]:
+                                    if sdval[1] in unique_items_cart:
+                                        cnt_discount = get_count(sdval[1])
+                                        if cnt_product > cnt_discount and cnt_product %cnt_discount == 0:
+                                            cnt_product = 0
+                                        else:
+                                            x = int(cnt_discount / sdval[0])
+                                            cnt_product-=x
+                                    else:
+                                        break
+                            tmp_price = int((cnt_product / val[0]) * val[1])
+                            cnt_product = cnt_product % val[0]
+                            discount_price +=tmp_price
+                            no_discount_price = int(cnt_product * dict_products[elem])
                         final_price = discount_price + no_discount_price
                     else:
                         final_price = cnt_product * dict_products[elem]
@@ -43,5 +57,11 @@ def checkout(skus):
         price = str(e)
 
     return price
+
+
+
+cart = 'AAAAAAAABBBCCCDDDDEEEEE'
+total_price = checkout(cart)
+print (total_price)
 
 
