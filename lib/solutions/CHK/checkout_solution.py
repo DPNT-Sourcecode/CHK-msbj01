de21490aa88ff044eab6e31a519d4cc050f34d2d
sbx_user1051 <sbx_user1051@169.254.4.69>
2019-11-09 13:26:08 +0000

    Sat Nov 09 13:26:08 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,16 +13,17 @@ def checkout(skus):
                 'A': 50,
                 'B': 30,
                 'C': 20,
-                'D': 15
+                'D': 15,
+                'E': 40
             }
-            dict_discount_quantity = {'A': 3, 'B': 2}
-            dict_discount_price = {'A': 130, 'B': 45}
+            dict_discount_quantity = {'A': [(5,200), (3,130)], 'B': [(2, 45)]}
+            special_discount = {'B': [(2,'E')]}
 
             unique_items_cart = ''.join(set(skus))
             for elem in unique_items_cart:
                 if elem in dict_products:
-                    final_price = 0
-                    cnt_product = skus.count(elem)
+                    final_price = tmp_price = discount_price = 0
+                    cnt_product = get_count(elem)
                     if elem in dict_discount_quantity:
                         discount_price =  int(int(cnt_product / dict_discount_quantity[elem]) * dict_discount_price[elem])
                         no_discount_price = int((cnt_product % dict_discount_quantity[elem]) * dict_products[elem])
@@ -42,4 +43,5 @@ def checkout(skus):
         price = str(e)
 
     return price
+
 
