9547b5d865e5d16a74554ad9741a2b524359cd67
sbx_user1051 <sbx_user1051@169.254.36.197>
2019-11-10 12:07:12 +0000

    Sun Nov 10 12:07:12 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -57,13 +57,14 @@ def checkout(skus):
 
     return price
 
-#cart = 'AAAAAAAABBBCCCDDDDEEEEEFFFFFGGGGGHHHHHHHHHHHHIIIJJJJKKKKKLLLMMMMMMNNNNNOOOOPPPPPQQQQRRRRRSSSTTTTUUUUUVVVVVWWWWWXXXXXYYYYYZZZZZ'
-cart = 'ZZZZZ'
+cart = 'AAAAAAAABBBCCCDDDDEEEEEFFFFFGGGGGHHHHHHHHHHHHIIIJJJJKKKKKLLLMMMMMMNNNNNOOOOPPPPPQQQQRRRRRSSSTTTTUUUUUVVVVVWWWWWXXXXXYYYYYZZZZZ'
+#cart = 'ZZZZZ'
 prc = checkout(cart)
 print(prc)
 
 # {"method":"checkout","params":["NNNM"],"id":"CHK_R4_106"}, expected: 120, got: 135
 #  {"method":"checkout","params":["NNNNM"],"id":"CHK_R4_107"}, expected: 160, got: 175
 #  {"method":"checkout","params":["NNNNNNMM"],"id":"CHK_R4_108"}, expected: 240, got: 270
+
 
 
